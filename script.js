ca77dcb318c4e9201134a92919196041e1eac035
Naam <naam.pt@gmail.com>
2020-03-16 21:21:37 -0400

    almost done with the first project

--- script.js
+++ script.js
@@ -1,77 +1,94 @@
-// var text = document.getElementById('time')
-// var today = new Date();
-
-// text.innerText = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-// var myVar = 
-// setInterval(myTimer, 1000);
-
-// function myTimer() {
-//   var today = new Date();
-//   document.getElementById("time").innerHTML = today.toLocaleTimeString();
-// }
-
-// setInterval(myTimer, 1000);
-
-// function myTimer() {
-//   var d = new Date();
-//   document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-// }
+//1. Make a clock to tick
 
 function myTimer() {
-    var today = new Date();
-    var text = document.getElementById("demo")
-    text.innerHTML = today.toLocaleTimeString();
+    var text = document.getElementById('time');
+    text.innerHTML = new Date().toLocaleTimeString();
     // or console.log(${today.getHours()});
 }
 
 setInterval(myTimer, 1000);
 //-------------------------------------------------------------------
 
-//First input event - manipulate font-size
+//2. manipulate font-size with input field
 const input = document.getElementById('font-size');
-//const log = document.getElementById('values');
 
 input.addEventListener('input', updateValue);
 
 function updateValue(e) {
-    var text = document.getElementById("demo") //edit
+    var text = document.getElementById("time") //edit
     // log.textContent = e.target.value;
-    text.style.fontSize = e.target.value;
-    console.log(e.target.value);
-    console.log(text)
+    text.style.fontSize = e.target.value + "px";
+    // console.log(e.target.value);
+    // console.log(text);
 };
 
-//articles on changing font-size with px
-//https://csscreator.com/node/386
+//----------------------------------------------------------------------
+
+//3. icon color
 
 //----------------------------------------------------------------------
 
-//Do i need change type for font input
+// 4. switch toggle to show date
+var days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+};
+
+var months = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December",
+};
+
+const checkBox = document.getElementById("checkBox")
+
+checkBox.addEventListener("change", displayDate);
+
+function displayDate() {
+  var currentDate = document.getElementById("date");
+  if (checkBox.checked) {
+    currentDate.innerHTML = `${days[new Date().getDay()]}, ${months[new Date().getMonth()]} ${new Date().getDate()}`;
+    currentDate.style.display = "block";
+  } else {
+    currentDate.style.display = "none";
+  } 
+}
+
+//---------------------------------------------------------
+// 5. switch toggle for 12 hrs
 
 
-//----------------------------------------------------------------------
-
-//Changeing theme with JS
-//https://codepen.io/nevan/pen/kBItz
+//--------------------------------------------------------------------
 
 
 
-// just for fun //
-//1.grab the target Object
-// var button = document.querySelector('button')
-//2. create a fx when the obj is triggered > console log (submit)
-// function doSth() {
-//     console.log("submit");
-// }
-// button.addEventListener("click", () => console.log("submit"));
-// -------------------------
 
-//**awesome video for Event Listener
-//**https://www.youtube.com/watch?v=EaRrmOtPYTM&list=PLyuRouwmQCjnEupVi5lpP6VrLg-eO-Zcp
+//--------------------------------------------------------------------
+//https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
+
+//if else with event listener
+//https://stackoverflow.com/questions/27731812/simple-if-else-onclick-then-do
+
+//awesome video for Event Listener
+//https://www.youtube.com/watch?v=EaRrmOtPYTM&list=PLyuRouwmQCjnEupVi5lpP6VrLg-eO-Zcp
 
 //research on toggle
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode
+//https://codepen.io/nevan/pen/kBItz
 
 //research on setting font size for time
 //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
@@ -80,4 +97,6 @@ function updateValue(e) {
 //access the dom
 //https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript
 
-
+//display visibility: hidden
+//https://stackoverflow.com/questions/4929310/why-isnt-css-visibility-working
+//https://tympanus.net/codrops/css_reference/visibility/
